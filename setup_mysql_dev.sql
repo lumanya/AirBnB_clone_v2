a24dc69d71ca195511cabadaaedaf8200c968421
lumanya <shaibulumanya@gmail.com>
2023-04-22 20:53:26 +0300

    add flush privielges command

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,6 +7,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev';
 
 -- Grant privileges to datase
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant privileges
+FLUSH PRIVILEGES;
+-- Grant the SELECT privilege to the user hbnb_dev to performance_schema db
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
