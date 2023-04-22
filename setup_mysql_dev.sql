551b4dd1586435438d700b73061d36a694f18451
lumanya <shaibulumanya@gmail.com>
2023-04-22 20:57:10 +0300

    updated the passwd

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,9 @@
 -- Create Database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create Database User
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant privileges to datase
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
 -- Grant the SELECT privilege to the user hbnb_dev to performance_schema db
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
