a6b29eb82cc00dc1251c6b5482b02bc2bcc1f9da
lumanya <shaibulumanya@gmail.com>
2023-04-22 20:42:06 +0300

    MySQl setup developemnt

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Script that prepare a MySQl Server for the project
+-- Create Database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create Database User
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev';
+
+-- Grant privileges to datase
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant privileges
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
