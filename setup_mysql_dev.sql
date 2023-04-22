d05af5559bad9766993b9af79e1784bd402a2919
lumanya <shaibulumanya@gmail.com>
2023-04-22 20:55:13 +0300

    remove lines

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,8 @@
 -- Script that prepare a MySQl Server for the project
 -- Create Database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 -- Create Database User
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev';
-
 -- Grant privileges to datase
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
