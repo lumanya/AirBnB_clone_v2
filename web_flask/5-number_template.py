b483c85cf00a4f5916a827aa8635f06ac88f254f
lumanya <shaibulumanya@gmail.com>
2023-06-05 23:20:46 +0300

    add strict_slahes on route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -30,12 +30,12 @@ def display_python(text='is cool'):
     return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>')
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ Diplay html template only if n is integer"""
     return render_template('5-number.html', number=n)
