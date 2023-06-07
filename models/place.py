a72124200d156e71f076978f91f528e1cee7950a
lumanya <shaibulumanya@gmail.com>
2023-06-07 10:06:08 +0300

    filter of states and amenity

--- models/place.py
+++ models/place.py
@@ -6,23 +6,23 @@ from sqlalchemy import Column, String, DateTime, ForeignKey, Integer, Float, Tab
 from sqlalchemy.orm import relationship
 from os import getenv
 
-if getenv("HBNB_TYPE_STORAGE") == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', 
-                                 String(60), 
-                                 ForeignKey('places.id'),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id',
-                                 String(60), 
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True, nullable=False),
-                                 )
+# if getenv("HBNB_TYPE_STORAGE") == 'db':
+#     place_amenity = Table('place_amenity', Base.metadata,
+#                           Column('place_id', 
+#                                  String(60), 
+#                                  ForeignKey('places.id'),
+#                                  primary_key=True, nullable=False),
+#                           Column('amenity_id',
+#                                  String(60), 
+#                                  ForeignKey('amenities.id'),
+#                                  primary_key=True, nullable=False),
+#                                  )
 
 class Place(BaseModel, Base):
     """ A place to stay """
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        # city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
@@ -33,8 +33,8 @@ class Place(BaseModel, Base):
         latitude = Column(Float(), nullable=True)
         longitude = Column(Float(), nullable=True)
         reviews = relationship('Review', cascade="all, delete", backref='place')
-        amenities = relationship('Amenity', secondary='place_amenity',
-                                 viewonly=False, backref='place_amenities')
+        # amenities = relationship('Amenity', secondary='place_amenity',
+        #                          viewonly=False, backref='place_amenities')
         # amenity_ids = []
     else:
         city_id = ""
